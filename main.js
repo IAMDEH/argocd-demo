1ff41b08a3fe89a2f661d7d257c0b95346c47bfa
IAMDEH <ee_izri@esi.dz>
2020-03-03 02:37:15 +0000

    Update main.js

--- main.js
+++ main.js
@@ -4,15 +4,16 @@ const hostname = '0.0.0.0';
 const port = 8080;
 
 console.log(process.env.ENVIRONMENT || 'e2e');
+console.log(process.env.ENVIRONMENT || 'prod');
 
-if (process.env.ENVIRONMENT === 'prod') {	
+/*if (process.env.ENVIRONMENT === 'prod') {	
     process.exit(1);	
-}
+}*/
 
 const server = http.createServer((_, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, Jenkins World!\n');
+  res.end('Hello, World!\n');
 });
 
 
